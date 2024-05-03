1b6e9f4134048c0493b73301d3e867620d6cefd5
Jan Vilaplana Bages <jan.vilaplana@iaballester.cat>
2024-05-03 09:32:53 +0200

    v0.1.8

--- tetris_menu.py
+++ tetris_menu.py
@@ -4,6 +4,7 @@ import time, pygame, random, sys
 HEIGHT, WIDTH = 640, 480
 BACKGROUND_IMAGE = 'assets/background/menu_background_1.png'
 credit = ''
+boing = 'Alberto Vellon'
 #
 pygame.init()
 screen = pygame.display.set_mode((HEIGHT, WIDTH))
@@ -12,7 +13,9 @@ def print_screen_backround(image):
     backround = pygame.image.load(image).convert()
     screen.blit(backround, (0, 0))
 #
-def credit_animation():
+x = 0
+#
+def credit_animation(x):
     print_screen_backround(BACKGROUND_IMAGE)
     pygame.display.update()
     #
@@ -23,36 +26,43 @@ def credit_animation():
         #
         print_screen_backround(BACKGROUND_IMAGE)
         img = font.render(credit, True, (i + 50, i + i, i))
-        screen.blit(img, (20, 200 - i))
+        screen.blit(img, (x, 200 - i))
         #
         pygame.display.update()
     #
 time.sleep(0.7)
 #
-for n in range(8):
+while True:
     credit = 'Sprites per:'
-    credit_animation()
+    credit_animation(210)
     #
     credit = 'Unai O. Pujol'
-    credit_animation()
+    credit_animation(200)
     #
     credit = 'Codi per:'
-    credit_animation()
-    #
-    credit = 'Unai O. Pujol:'
-    credit_animation()
-    #
-    credit = 'Musica per:'
-    credit_animation()
+    credit_animation(240)
     #
     credit = 'Unai O. Pujol'
-    credit_animation()
+    credit_animation(200)
+    #
+    credit = 'Musica per:'
+    credit_animation(220)
+    #
+    credit = 'Unai O. Pujol'
+    credit_animation(200)
     #
     credit = 'El millor profe del mon:'
-    credit_animation()
+    credit_animation(120)
     #
-    credit = 'Xavi Sancho'
-    credit_animation()
+    credit = boing
+    credit_animation(200)
     #
+    credit = 'Esta ayudando!!!:'
+    credit_animation(170)
+    #
+    credit = 'Jan Vilaplana'
+    credit_animation(190)
+    #
+    break
 #
 
